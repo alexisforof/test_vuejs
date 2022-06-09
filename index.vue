390a5a614a70a58b02142ed8ed94dd1321db6296
aouarab <aouarab@awe.fr>
2022-06-09 14:07:43 +0200

    first commit

---
+++ index.vue
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        user: "Paola",
+        message: "Entrer votre nom",
+        produits: ['Pizza', 'Burger', 'Frites'],
+        command: []
+    },
+    methods: {
+        commander: function(produit) {
+            this.command.push(produit);
+            
+        }
+    }
+})
